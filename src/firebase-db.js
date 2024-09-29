31ccf2c4cc987c798b6d2f9ccb3333231ffa0c76
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-09-29 22:44:02 +0300

    update firebase-db, process.env

--- src/firebase-db.js
+++ src/firebase-db.js
@@ -10,15 +10,14 @@ import {
 } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCdUj8puEDK2rJrF4bWiuZ5z9Jkc-y0SLc',
-  authDomain: 'psychologists-services-2e99e.firebaseapp.com',
-  databaseURL:
-    'https://psychologists-services-2e99e-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'psychologists-services-2e99e',
-  storageBucket: 'psychologists-services-2e99e.appspot.com',
-  messagingSenderId: '275061905720',
-  appId: '1:275061905720:web:7a8756677ea0eff05409cd',
-  measurementId: 'G-N2Q50N158Z',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
